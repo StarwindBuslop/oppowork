715d0e71a1d8c2aaf9a8fa545b2e214443a5464f
frbustlop@gmail.com <Francisco.Bustamante@DESKTOP-G6NG6HG>
2022-11-29 09:39:13 +0100

    Preparing project

---
+++ oppowork/src/main/java/com/oppowork/oppowork/OppoworkApplication.java
@@ -0,0 +1,13 @@
+package com.oppowork.oppowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppoworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppoworkApplication.class, args);
+	}
+
+}
---
+++ oppowork/src/test/java/com/oppowork/oppowork/OppoworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oppowork.oppowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OppoworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
